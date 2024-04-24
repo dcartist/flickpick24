e5aaf9a2de9a858de9dc6b20c1a089d10ee34fce
dcartist <pbannerman13@gmail.com>
2024-04-24 17:46:26 -0400

    updating display css

--- src/Components/Displays/Display.js
+++ src/Components/Displays/Display.js
@@ -1,18 +1,20 @@
-import React from 'react'
+import React, { useRef } from 'react';
 import { MDBLazyLoading, MDBContainer, MDBRow } from 'mdb-react-ui-kit';
 export default function DisplaySmall({ imageUrl, alt }) {
     // console.log(imageUrl)
     // console.log(alt)
+    const basicContainer = useRef(null);
   return (
     <div>  
 
 <MDBLazyLoading
+//  containerRef={basicContainer}
           lazySrc={`https://image.tmdb.org/t/p/original${imageUrl}`}
           lazyPlaceholder='https://place-hold.it/300x500/666/fff/000?text=loading...&fontsize=16'
         //   lazyPlaceholder='https://place-hold.it/1321x583?text=Loading'
         //   lazyDelay={1000}
           alt={alt}
-          className='img-fluid'
+          className='displayImages'
           animation='zoom-in'
         />
         {/* <img
