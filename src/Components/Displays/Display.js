423a25eeb08f401be878d55ce1fc183581925ec2
dcartist <pbannerman13@gmail.com>
2024-05-02 00:12:24 -0400

    updating Display

--- src/Components/Displays/Display.js
+++ src/Components/Displays/Display.js
@@ -2,7 +2,6 @@ import React, { useRef } from 'react';
 import { MDBLazyLoading, MDBContainer, MDBAnimation, MDBRow } from 'mdb-react-ui-kit';
 
 
-// export const DisplaySmall = function({ imageUrl, alt, starter }) {
 export default function DisplaySmall({ imageUrl, alt, starter }) {
   return (
     <div className="text-center">  
@@ -22,22 +21,3 @@ export default function DisplaySmall({ imageUrl, alt, starter }) {
   )
 }
 
-
-// export const DisplaySmallThumbs = function ({ imageUrl, alt, starter }) {
-//   return (
-//     <div className="text-center">  
-//              <MDBAnimation
-//             reset={true}
-//             tag='img'
-//             repeatOnScroll
-//             start={starter}
-//             animation='fade-in'
-//             duration={1000}
-//             delay={300}
-//             alt={alt}
-//             src={`https://image.tmdb.org/t/p/original${imageUrl}`}
-//             className='displayImagesThumb'
-//           />
-//   </div>
-//   )
-// }
