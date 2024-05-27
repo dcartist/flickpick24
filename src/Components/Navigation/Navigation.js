e54178640a0f3ff0e6c515c9fdc4e7bc83af0922
dcartist <pbannerman13@gmail.com>
2024-05-27 17:58:13 -0400

    updating navigation

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import {
   MDBContainer,
   MDBNavbar,
@@ -9,37 +9,49 @@ import {
   MDBNavbarItem,
   MDBNavbarLink,
   MDBCollapse,
-  MDBIcon
-} from 'mdb-react-ui-kit';
+  MDBIcon,
+} from "mdb-react-ui-kit";
 
 export default function Navigation() {
   const [openNav, setOpenNav] = useState(false);
 
   return (
-    <MDBNavbar expand='lg' dark bgColor='dark'>
+    <MDBNavbar expand="lg" dark bgColor="dark">
       <MDBContainer fluid>
-        <MDBNavbarBrand as={Link}  to='/home' className='title_header'>FlickPick</MDBNavbarBrand>
+        <MDBNavbarBrand as={Link} to="/home" className="title_header">
+          FlickPick
+        </MDBNavbarBrand>
         <MDBNavbarToggler
-          type='button'
-          aria-expanded='false'
-          aria-label='Toggle navigation'
+          type="button"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
           onClick={() => setOpenNav(!openNav)}
         >
-          <MDBIcon icon='bars' fas />
+          <MDBIcon icon="bars" fas />
         </MDBNavbarToggler>
         <MDBCollapse navbar open={openNav}>
           <MDBNavbarNav>
             <MDBNavbarItem>
-              <MDBNavbarLink aria-current='page'>
-                <Link to='../'  className="link-secondary text-white">
-                Home</Link>
+              <MDBNavbarLink aria-current="page">
+                <Link to="../" className="link-secondary text-white">
+                  Home
+                </Link>
               </MDBNavbarLink>
             </MDBNavbarItem>
             <MDBNavbarItem>
-  <MDBNavbarLink as={Link}  to='/listing'><Link to='/listing'  className="link-secondary text-white">Listing</Link></MDBNavbarLink>
-  <MDBNavbarLink as={Link}  to='/search'><Link to='/search'  className="link-secondary text-white">Search</Link></MDBNavbarLink>
-</MDBNavbarItem>
-         
+              <MDBNavbarLink as={Link} to="/listing">
+                <Link to="/listing" className="link-secondary text-white">
+                  Listing
+                </Link>
+              </MDBNavbarLink>
+            </MDBNavbarItem>
+            <MDBNavbarItem>
+              <MDBNavbarLink as={Link} to="/search">
+                <Link to="/search" className="link-secondary text-white">
+                  Search
+                </Link>
+              </MDBNavbarLink>
+            </MDBNavbarItem>
           </MDBNavbarNav>
         </MDBCollapse>
       </MDBContainer>
