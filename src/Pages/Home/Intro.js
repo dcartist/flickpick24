de9078fe1d2b961293f98f214f67a12b9bcbdd27
dcartist <pbannerman13@gmail.com>
2025-05-29 23:31:58 -0400

    adding popcorn

--- src/Pages/Home/Intro.js
+++ src/Pages/Home/Intro.js
@@ -1,39 +1,42 @@
 import React from 'react'
 import { motion } from 'framer-motion';
 import movieReel from "../../Components/Image/movieReel.png"
+import movieBackground from "../../Components/Image/popcorn.png"
 
 export default function Intro() {
+  const styles = {
+    background: `url(${movieBackground}), linear-gradient(180deg, rgba(161, 0, 0, 1) 7%, rgba(3, 0, 0, 1) 94%)`,
+    backgroundSize: 'cover',
+    backgroundPosition: 'top left',
+    height: '100vh',
+    width: '100%',
+    borderTop: '5px solid #000',
+    position: 'relative',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+  }
   return (
-    <div className="d-flex justify-content-center align-items-center p-5 container-75">
-        
-        {/* <motion.img
-          src={movieReel}
-          alt="Movie Reel"
-          animate={{ rotate: [0, 360], 
-            scale: [.9, 1, .9], 
-            y: [0, -20, 0]  }} 
-          transition={{
-            rotate: { duration: 3, repeat: Infinity, ease: "linear" },
-            scale: { duration: 2, repeat: Infinity, ease: "easeInOut" },
-            // y: { duration: 3, repeat: Infinity, ease: "easeInOut" } 
-          }}
-          style={{
-            position: 'relative',
-            top: '-50px',
-            maxHeight: '300px',
-            filter: 'drop-shadow(0 10px 10px rgba(0,0,0,0.5))'
-          }}
-        /> */}
-        <p className='title_header'>FlickPick</p>
-
-        {/* <img src={movieReel} alt="movie" /> */}
-        {/* <motion.img
+    <div className="intro-Background" style={styles}>
+      {/* Example animated image, uncomment if you want it */}
+      {/* 
+      <motion.img
         src={movieReel}
         alt="Movie Reel"
-        animate={{ rotate: 360 }}
-        transition={{ duration: 2, repeat: Infinity, ease: "linear" }}
-        style={{ position: 'relative', top: '-50px', y: ['0px', '-20px', '0px'], x: ['0px', '20px', '0px'] }}
-      /> */}
+        animate={{ rotate: [0, 360], scale: [.9, 1, .9], y: [0, -20, 0] }} 
+        transition={{
+          rotate: { duration: 3, repeat: Infinity, ease: "linear" },
+          scale: { duration: 2, repeat: Infinity, ease: "easeInOut" },
+        }}
+        style={{
+          position: 'relative',
+          top: '-50px',
+          maxHeight: '300px',
+          filter: 'drop-shadow(0 10px 10px rgba(0,0,0,0.5))'
+        }}
+      />
+      */}
+      <p className='title_header'>FlickPick</p>
     </div>
   )
 }
